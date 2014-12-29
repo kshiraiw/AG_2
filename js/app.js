fbadc9fd4ee9ec92c2d90435ff1d552802a8206d
Kiyomi Li <kshiraiw@gmail.com>
2014-12-29 09:54:18 -0800

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+$(document).foundation({
+	orbit: {
+		slide_number: false,
+		variable_height: true,
+		timer: false,
+		bullets: false
+	}
+});
